f991078c41700021d87dc6ca2070b3373ebc4ad0
insanjay <insanjay.work@gmail.com>
2025-09-06 22:39:58 +0530

    added the pwa functionality

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// Basic service worker for caching essential assets
+const CACHE_NAME = 'flowchat-v1';
+const urlsToCache = [
+  '/flowchat/',
+  '/flowchat/index.html',
+  '/flowchat/static/css/style.css',
+  '/flowchat/static/js/chat.js',
+  '/flowchat/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
